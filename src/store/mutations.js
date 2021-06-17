e15e9a90926a7c0a59a393290397cbdc0b24f979
YGJ <1150009882@qq.com>
2021-06-17 11:13:52 +0800

    拆分vuex组件初始

---
+++ src/store/mutations.js
@@ -0,0 +1,35 @@
+export default {
+    // // 一般mutations不做逻辑处理，一般放在action中处理，通过调用action修改state状态
+    // addCart(state, payload) {
+    //     // 下面的是旧版本可行
+    //     //payload新添加的商品
+    //     // let oldproduct = null;
+    //     // for (let item of state.cartList) {
+    //     //     if (item.iid === payload.iid) {
+    //     //         oldproduct = item
+    //     //     }
+    //     // }
+
+    //     // 下面的版本也可行
+    //     let oldproduct = state.cartList.find(item => item.iid === payload.iid)
+
+
+
+    //     // 判断oldproduct
+    //     if (oldproduct) {
+    //         oldproduct.count += 1
+    //     } else {
+    //         payload.count = 1;
+    //         state.cartList.push(payload)
+    //     }
+
+    // }
+
+
+    addCount(state, payload) {
+        payload.count++;
+    },
+    addToCart(state, payload) {
+        state.cartList.push(payload)
+    }
+}
